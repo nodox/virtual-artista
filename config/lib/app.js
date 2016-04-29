b371a9bc39f18cb71a47a953db5c0a4740f00738
nodox <snatera09@gmail.com>
2016-04-28 21:33:16 -0500

    angularjs working

--- config/lib/app.js
+++ config/lib/app.js
@@ -4,7 +4,10 @@
  * Module dependencies.
  */
  var express = require('express'),
- path = require('path');
+ 		 bodyParser = require('body-parser'),
+	   morgan = require('morgan'),
+	   methodOverride = require('method-override'), // simulate DELETE and PUT (express4)
+     path = require('path');
 
 
 module.exports.start = function start() {
@@ -15,7 +18,13 @@ module.exports.start = function start() {
     var port = 5000;
     var host = 'localhost';
 
-    app.use(express.static('../../bower_components'));
+    app.use(express.static(path.resolve('./modules/core')));
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
 
     // Send requests to angular for routing
     app.get('*', function (req, res) {
---
+++ modules/core/core.js
@@ -0,0 +1,10 @@
+'use-strict';
+
+var app = angular.module('virtualArtista', []);
+
+
+
+app.controller('CoreController', function ($scope, $http) {
+	$scope.greeting = 'Yay. Angular Data-Binding!';
+
+});
